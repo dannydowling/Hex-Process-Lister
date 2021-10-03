7343ddc8544ca36dd4e9b326777ed1e988ffe48e
danny <danny.dowling@gmail.com>
2021-10-03 04:29:39 -0800

    Made a process checker for if the system is slow.

--- HexProcessLister2/Program.cs
+++ HexProcessLister2/Program.cs
@@ -1,19 +1,35 @@
 ﻿using System.Diagnostics;
 
-public class hexLister
-{
-    public record ProcessRecord(string ProcessName, string ProcessId);
+public class ProcessChecker
+{    
+    public record ProcessRecord(string ProcessName,bool Responding, string Id, string HandleCount);
 
     public static void Main()
     {
         Process[] processList = Process.GetProcesses();
+        List<ProcessRecord> list = new List<ProcessRecord>();
 
         foreach (var process in processList)
         {
+            //iterate the processes in the list and create a new record for each
             ProcessRecord processRecord = new(process.ProcessName,
-                                                                           (Convert.ToString((process.Id), 16)));
-            Console.WriteLine(processRecord);
-            // output: Process { ProcessName = csrss, ProcessId = 18F4 }
+                                                                           process.Responding,
+                                                                           (Convert.ToString((process.Id), 16)),
+                                                                           Convert.ToString(process.HandleCount));
+            list.Add(processRecord);
+        }
+
+        foreach (var processRecord in list)
+        {
+            if (processRecord.Responding == false)
+            {
+                string output = (
+                   ($"Process:", processRecord.ProcessName),
+                   ($"Number of Handles:", processRecord.HandleCount),
+                  ($"Process Id in Hex:", processRecord.Id)).ToString();
+
+                Console.WriteLine(output);
+            }
         }
     }
 }
